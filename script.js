95be6c220a4cd6f4356c047cd6b78c3b71a2240a
Juan Pablo Rigotti <dailutb@gmail.com>
2022-03-20 15:22:43 -0300

    adjusting JS

--- script.js
+++ script.js
@@ -6,7 +6,14 @@ function compute()
     var years = document.getElementById("years").value;
     var amount = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
+
+    if (principal != "" && principal > 0)
+        {
     document.getElementById("result").innerHTML="If you deposit "+principal+" USD,\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+" USD,\<br\>in the year "+year+"\<br\>"
+        }
+        else
+        {alert('You must enter an valid Amount');    
+        }
 }
         
 
@@ -16,6 +23,11 @@ function updateRate()
     document.getElementById("rate_val").innerText=rateval;
 }
 
+
+function clear_result(){
+    document.getElementById("principal").value="";
+    document.getElementById("result").innerHTML="";
+}
 /*
 function readRate(){
     var rate = document.getElementById('rate');
