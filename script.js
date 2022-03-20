10c449ca09bfb2e61fbb6fc33c280873a2ae227c
Juan Pablo Rigotti <dailutb@gmail.com>
2022-03-20 12:55:58 -0300

    Adding style and JS

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var amount = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit "+principal+" USD,\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+" USD,\<br\>in the year "+year+"\<br\>"
 }
         
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+/*
+function readRate(){
+    var rate = document.getElementById('rate');
+    return rate.value;
+}
+*/
